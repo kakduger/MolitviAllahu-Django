1439519cbb276eeb4f78fea3f33b38b556367caf
Zahar123456 <kakaduger2.0@gmail.com>
2022-03-13 17:56:57 +0600

    ALLAAAAAH

---
+++ Magic/welcomeup/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def initPage(request):
+    return render(request, "index.html")
+    
+def aboutPage(request):
+    return HttpResponse("about")
